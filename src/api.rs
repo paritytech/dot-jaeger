5a18a11c6e0c9729d999bd5bbff1063e57336aa3
Andrew Plaza <aplaza@liquidthink.net>
2021-02-05 12:34:10 +0100

    run rustfmt

--- src/api.rs
+++ src/api.rs
@@ -17,8 +17,8 @@
 //! Rust Code wrapping Jaeger-Agent HTTP API
 
 use crate::{
-    cli::{AllTraces, App, Services, Trace},
-    primitives::{RpcResponse, TraceObject},
+	cli::{AllTraces, App, Services, Trace},
+	primitives::{RpcResponse, TraceObject},
 };
 use anyhow::Error;
 use std::fmt;
@@ -41,79 +41,75 @@ pub const TRACES: &str = "/api/traces";
 pub const SERVICES: &str = "/api/services";
 
 pub enum Endpoint {
-    Traces,
-    Services,
+	Traces,
+	Services,
 }
 
 impl fmt::Display for Endpoint {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Endpoint::Traces => write!(f, "{}", TRACES),
-            Endpoint::Services => write!(f, "{}", SERVICES),
-        }
-    }
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Endpoint::Traces => write!(f, "{}", TRACES),
+			Endpoint::Services => write!(f, "{}", SERVICES),
+		}
+	}
 }
 
 pub struct JaegerApi<'a> {
-    /// URL Where Jaeger Agent is running.
-    /// Should be full URL including Port and protocol.
-    /// # Example
-    /// http://localhost:16686
-    url: &'a str,
+	/// URL Where Jaeger Agent is running.
+	/// Should be full URL including Port and protocol.
+	/// # Example
+	/// http://localhost:16686
+	url: &'a str,
 }
 
 impl<'a> JaegerApi<'a> {
-    pub fn new(url: &'a str) -> Self {
-        Self { url }
-    }
+	pub fn new(url: &'a str) -> Self {
+		Self { url }
+	}
 
-    /// Get many traces belonging to one service from this Jaeger Agent.
-    pub fn traces(&self, app: &App, all_traces: &AllTraces) -> Result<Vec<TraceObject>, Error> {
-        let req = ureq::get(&endpoint(self.url, Endpoint::Traces));
-        let req = build_parameters(req, app);
-        let response: RpcResponse<TraceObject> = req.call()?.into_json()?;
-        Ok(response.consume())
-    }
+	/// Get many traces belonging to one service from this Jaeger Agent.
+	pub fn traces(&self, app: &App, all_traces: &AllTraces) -> Result<Vec<TraceObject>, Error> {
+		let req = ureq::get(&endpoint(self.url, Endpoint::Traces));
+		let req = build_parameters(req, app);
+		let response: RpcResponse<TraceObject> = req.call()?.into_json()?;
+		Ok(response.consume())
+	}
 
-    /// Get a single trace from the Jaeger Agent
-    pub fn trace(&self, app: &App, trace: &Trace) -> Result<TraceObject, Error> {
-        // /api/traces/{trace_id}
-        let req = ureq::get(&format!(
-            "{}/{}",
-            &endpoint(self.url, Endpoint::Traces),
-            trace.id.to_string()
-        ));
-        let req = build_parameters(req, app);
-        let response: RpcResponse<TraceObject> = req.call()?.into_json()?;
-        // if the response is succesful we should have exactly 1 item
-        Ok(response.consume().remove(0))
-    }
+	/// Get a single trace from the Jaeger Agent
+	pub fn trace(&self, app: &App, trace: &Trace) -> Result<TraceObject, Error> {
+		// /api/traces/{trace_id}
+		let req = ureq::get(&format!("{}/{}", &endpoint(self.url, Endpoint::Traces), trace.id.to_string()));
+		let req = build_parameters(req, app);
+		let response: RpcResponse<TraceObject> = req.call()?.into_json()?;
+		// if the response is succesful we should have exactly 1 item
+		Ok(response.consume().remove(0))
+	}
 
-    /// Query the services that reporting to this Jaeger Agent
-    pub fn services(&self, app: &App, services: &Services) -> Result<Vec<String>, Error> {
-        let req = ureq::get(&endpoint(&self.url, Endpoint::Services));
-        let req = build_parameters(req, app);
-        let response: RpcResponse<String> = req.call()?.into_json()?;
-        Ok(response.consume())
-    }
+	/// Query the services that reporting to this Jaeger Agent
+	pub fn services(&self, app: &App, services: &Services) -> Result<Vec<String>, Error> {
+		let req = ureq::get(&endpoint(&self.url, Endpoint::Services));
+		let req = build_parameters(req, app);
+		let response: RpcResponse<String> = req.call()?.into_json()?;
+		Ok(response.consume())
+	}
 }
 
 /// Applies a regex filter to the arguments
 fn apply_filter(input: String) -> String {
-    todo!();
+	todo!();
 }
 
 fn build_parameters(req: ureq::Request, app: &App) -> ureq::Request {
-    ParamBuilder::new()
-        .service(app.service.as_deref())
-        .limit(app.limit)
-        .pretty_print(app.pretty_print)
-        .lookback(app.lookback.as_deref())
-        .build(req)
+	ParamBuilder::new()
+		.service(app.service.as_deref())
+		.limit(app.limit)
+		.pretty_print(app.pretty_print)
+		.lookback(app.lookback.as_deref())
+		.build(req)
 }
 
 fn endpoint(url: &str, endpoint: Endpoint) -> String {
-    format!("{}/{}", url, endpoint)
+	format!("{}/{}", url, endpoint)
 }
 
 // TODO: Params to Implement
@@ -123,61 +119,56 @@ fn endpoint(url: &str, endpoint: Endpoint) -> String {
 // start <- Unix timestamp in microseconds (presumably for internal Jaeger Use)
 // end <- Unix timestamp in microseconds (presumably for internal Jaeger Use)
 pub struct ParamBuilder<'a> {
-    limit: Option<usize>,
-    pretty_print: bool,
-    service: Option<&'a str>,
-    lookback: Option<&'a str>,
+	limit: Option<usize>,
+	pretty_print: bool,
+	service: Option<&'a str>,
+	lookback: Option<&'a str>,
 }
 
 impl<'a> ParamBuilder<'a> {
-    pub fn new() -> Self {
-        Self {
-            pretty_print: false,
-            limit: None,
-            service: None,
-            lookback: None,
-        }
-    }
+	pub fn new() -> Self {
+		Self { pretty_print: false, limit: None, service: None, lookback: None }
+	}
 
-    /// Amount of JSON objects to return in one GET.
-    pub fn limit(mut self, limit: Option<usize>) -> Self {
-        self.limit = limit;
-        self
-    }
+	/// Amount of JSON objects to return in one GET.
+	pub fn limit(mut self, limit: Option<usize>) -> Self {
+		self.limit = limit;
+		self
+	}
 
-    /// Pretty print the JSON results.
-    pub fn pretty_print(mut self, pretty_print: bool) -> Self {
-        self.pretty_print = pretty_print;
-        self
-    }
+	/// Pretty print the JSON results.
+	pub fn pretty_print(mut self, pretty_print: bool) -> Self {
+		self.pretty_print = pretty_print;
+		self
+	}
 
-    /// Specify the service that should be queried from the Jaeger Agent.
-    pub fn service(mut self, service: Option<&'a str>) -> Self {
-        self.service = service;
-        self
-    }
+	/// Specify the service that should be queried from the Jaeger Agent.
+	pub fn service(mut self, service: Option<&'a str>) -> Self {
+		self.service = service;
+		self
+	}
 
-    /// How far back to look for traces.
-    pub fn lookback(mut self, lookback: Option<&'a str>) -> Self {
-        self.lookback = lookback;
-        self
-    }
+	/// How far back to look for traces.
+	pub fn lookback(mut self, lookback: Option<&'a str>) -> Self {
+		self.lookback = lookback;
+		self
+	}
 
-    pub fn build(self, req: ureq::Request) -> ureq::Request {
-        let mut req = req.query("prettyPrint", &self.pretty_print.to_string());
+	pub fn build(self, req: ureq::Request) -> ureq::Request {
+		let mut req = req.query("prettyPrint", &self.pretty_print.to_string());
 
-        if let Some(service) = self.service {
-            req = req.query("service", &service.to_string());
-        }
+		if let Some(service) = self.service {
+			req = req.query("service", &service.to_string());
+		}
 
-        if let Some(limit) = self.limit {
-            req = req.query("limit", &limit.to_string());
-        }
+		if let Some(limit) = self.limit {
+			req = req.query("limit", &limit.to_string());
+		}
 
-        if let Some(lookback) = self.lookback {
-            req = req.query("lookback", &lookback.to_string());
-        }
+		if let Some(lookback) = self.lookback {
+			req = req.query("lookback", &lookback.to_string());
+		}
 
-        req
-    }
+		req
+	}
 }
