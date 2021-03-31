108158a1b8e1a35e7b00e10e356b2fb90e1f5a97
Andrew Plaza <andrew.plaza@parity.io>
2021-03-31 13:20:17 +0200

    cleanup + tests

--- src/graph.rs
+++ src/graph.rs
@@ -16,7 +16,7 @@
 
 use crate::primitives::{Span, TraceObject};
 use anyhow::{Context, Error};
-use daggy::{Dag, EdgeIndex, NodeIndex, Walker};
+use daggy::{Dag, NodeIndex, Walker};
 use petgraph::visit::{Control, DfsEvent};
 use std::collections::HashMap;
 
@@ -59,42 +59,23 @@ impl<'a> Graph<'a> {
 	{
 		let span_node = self.index_lookup.get(id).context(format!("Failed to lookup index for span {}", id))?;
 		petgraph::visit::depth_first_search(&self.graph, Some(*span_node), |event| match event {
-			DfsEvent::TreeEdge(u, v) => {
-				if fun(&self.graph.raw_nodes()[u.index()].weight) || fun(&self.graph.raw_nodes()[v.index()].weight) {
+			DfsEvent::TreeEdge(_, v) => {
+				if fun(&self.graph.raw_nodes()[v.index()].weight) {
 					return Control::Break(v);
 				} else {
 					Control::Continue
 				}
-			}
-			DfsEvent::Finish(_, t) => {
-				log::debug!("Took {:?} for a depth-first search of child spans", t);
-				Control::Continue
 			}
 			_ => Control::Continue,
 		});
 		Ok(())
 	}
 
-	// FIXME: This can be improved further but
-	// it is of questionable worth as it would probably require a more complicated algorithm to figure out which
-	// child path to go down; maybe one of the breadth/depth searches.
-	// In testing the Polkadot validators rarely had a child span, and if they did
-	// I never found an instance when it was more than one. However it could be worth doing to future-proof dot-jaeger
-	// if it receives lots of use and parachain code/jaeger changes in some way.
-	/// Iterate through the first child of each span
-	/// Most Jaeger spans have only one child.
-	pub fn children(&'a self, id: &'a str) -> Option<impl Iterator<Item = &'a Span<'a>>> {
-		let id = self.index_lookup.get(id)?;
-		let iter = self.graph.recursive_walk(*id, move |rgraph, idx| {
-			rgraph.children(idx).iter(&rgraph).collect::<Vec<(EdgeIndex, NodeIndex)>>().get(0).map(|s| *s)
-		});
-
-		Some(iter.iter(&self.graph).map(move |(_, n)| &self.graph.raw_nodes()[n.index()].weight))
-	}
-
+	// recursive walk through parents of a span
 	pub fn parents(&'a self, id: &'a str) -> Option<impl Iterator<Item = &'a Span<'a>>> {
 		let id = self.index_lookup.get(id)?;
-		Some(self.graph.parents(*id).iter(&self.graph).map(move |(_, n)| &self.graph.raw_nodes()[n.index()].weight))
+		let iter = self.graph.recursive_walk(*id, |rgraph, n| rgraph.parents(n).iter(&rgraph).nth(0));
+		Some(iter.iter(&self.graph).map(move |(_, n)| &self.graph.raw_nodes()[n.index()].weight))
 	}
 }
 
@@ -102,25 +83,17 @@ impl<'a> Graph<'a> {
 mod tests {
 	use super::*;
 	use crate::tests::*;
-	use petgraph::dot::{Config, Dot};
-
-	#[test]
-	fn output_graphviz() {
-		let traces: TraceObject = serde_json::from_str(TEST_DATA).unwrap();
-		let graph = Graph::new(&traces).unwrap();
-		let inner = graph.graph.into_graph();
-		let viz = Dot::with_config(&inner, &[Config::EdgeNoLabel]);
-		println!("{:?}", viz);
-	}
 
 	#[test]
 	fn should_iter_parents() -> Result<(), Error> {
 		let traces: TraceObject = serde_json::from_str(TEST_DATA)?;
 		let graph = Graph::new(&traces)?;
 
-		let mut iterator = graph.parents("child-1").unwrap();
+		let mut iterator = graph.parents("child-2").unwrap();
+		assert_eq!(Some("child-1"), iterator.next().map(|s| s.span_id));
 		assert_eq!(Some("child-0"), iterator.next().map(|s| s.span_id));
-		//	assert_eq!(Some("parent"), iterator.next().map(|s| s.span_id));
+		assert_eq!(Some("parent"), iterator.next().map(|s| s.span_id));
+		assert_eq!(None, iterator.next().map(|s| s.span_id));
 		Ok(())
 	}
 
@@ -129,13 +102,13 @@ mod tests {
 		let traces: TraceObject = serde_json::from_str(TEST_DATA)?;
 		let graph = Graph::new(&traces)?;
 
-		// let mut iterator = graph.children("parent").unwrap();
-		// assert_eq!(Some("child-0"), iterator.next().map(|s| s.span_id));
-		// assert_eq!(Some("child-1"), iterator.next().map(|s| s.span_id));
+		let mut checked = Vec::new();
+		graph.search("parent", |span| {
+			checked.push(span.span_id);
+			false
+		})?;
 
-		for thing in graph.children("parent").unwrap() {
-			println!("{}", thing.span_id);
-		}
+		assert_eq!(vec!["child-0", "child-1", "child-2"], checked);
 		Ok(())
 	}
 }
